c3c43f6e3e32e65cae91f73a0201cdc64b4ac5d1
TonSuchat <suchat.v26@gmail.com>
2021-08-29 18:25:44 +0700

    fix initial socket

--- client-side/src/App.tsx
+++ client-side/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useContext } from "react";
+import { useState, useContext, useEffect } from "react";
 import SocketContext, { SocketContextType } from "./contexts/socketContext";
 import withSocket from "./utils/withSocket";
 
@@ -6,13 +6,15 @@ const App = () => {
   const { socket } = useContext<SocketContextType>(SocketContext);
   const [msg, setMsg] = useState<string>("");
 
+  useEffect(() => {
+    socket.on("sendMessage", (arg) => {
+      console.log(arg);
+    });
+  }, [socket]);
+
   const onSendClicked = () => {
     socket.emit("sendMessage", msg);
   };
-
-  socket.on("sendMessage", (arg) => {
-    console.log(arg);
-  });
 
   return (
     <div>
